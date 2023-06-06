67292d7637fb925519db48a458506f4031a1d4cf
Matthew Peveler <matt@popsql.com>
2023-06-06 07:17:14 -0400

    Retry for 502-504 codes, fail on >= 400 codes

--- index.spec.js
+++ index.spec.js
@@ -1,7 +1,7 @@
 // Before running the tests, make sure to start the docker containers
 
-var { describe, expect, test } = require('@jest/globals');
-var os = require('os');
+var { beforeAll, beforeEach, afterAll, describe, expect, test } = require('@jest/globals');
+var http = require('http');
 var Client = require('./index').Client;
 
 test('cannot use basic and custom auth', function(){
@@ -54,6 +54,158 @@ describe.each([['presto'], ['trino']])('%s', function(engine){
         expect(error.message).toEqual('line 1:15: Table ' + tableName + ' does not exist');
         done();
       },
+    });
+  });
+});
+
+describe('non-200 codes', function(){
+  describe('retry logic for 50x errors', function(){
+    describe.each([502, 503, 504])('retry logic for %i', function(statusCode){
+      var responses = {
+        '/v1/statement': {
+            "stats": {
+                "state": "QUEUED",
+            },
+            "nextUri": "http://localhost:8111/v1/statement/20140120_032523_00000_32v8g/1",
+            "infoUri": "http://localhost:8111/v1/query/20140120_032523_00000_32v8g",
+            "id": "20140120_032523_00000_32v8g",
+        },
+        '/v1/statement/20140120_032523_00000_32v8g/1': {
+          "stats": {
+              "state": "FINISHED",
+          },
+          "columns": [ { "type": "integer", "name": "col" } ],
+          "data": [ [ 1 ] ],
+          "infoUri": "http://localhost:8111/v1/query/20140120_032523_00000_32v8g",
+          "id": "20140120_032523_00000_32v8g"
+        }
+      };
+
+      var server;
+      var count;
+
+      beforeAll(function(done) {
+        server = http.createServer(function(req, res){
+          if (count % 5 === 0) {
+            count++;
+            res.statusCode = statusCode;
+          } else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.write(JSON.stringify(responses[req.url]));
+          }
+          res.end();
+        });
+        server.listen(8111, function(){
+          done();
+        });
+      });
+
+      beforeEach(function(){
+        count = 0;
+      });
+
+      afterAll(function(done){
+        server.close(function(){
+          done();
+        });
+      });
+
+      test('that client retries', function(done){
+        var client = new Client({
+          host: 'localhost',
+          port: 8111,
+        });
+        client.execute({
+          query: 'SELECT 1 AS col',
+          data: function(error, data, columns){
+            expect(error).toBeNull();
+            expect(data).toEqual([[1]]);
+            expect(columns).toHaveLength(1);
+            expect(columns[0]).toEqual(expect.objectContaining({ name: 'col', type: 'integer' }));
+          },
+          callback: function(error){
+            expect(error).toBeNull();
+            done();
+          },
+        });
+      });
+    });
+  });
+
+  describe.each([404, 500])('query errors for %i code', function(statusCode){
+    describe.each([0, 1])('fail after %i requests', function(failAfter){
+      var responses = {
+        '/v1/statement': {
+            "stats": {
+                "state": "QUEUED",
+            },
+            "nextUri": "http://localhost:8111/v1/statement/20140120_032523_00000_32v8g/1",
+            "infoUri": "http://localhost:8111/v1/query/20140120_032523_00000_32v8g",
+            "id": "20140120_032523_00000_32v8g",
+        },
+        '/v1/statement/20140120_032523_00000_32v8g/1': {
+          "stats": {
+              "state": "FINISHED",
+          },
+          "columns": [ { "type": "integer", "name": "col" } ],
+          "data": [ [ 1 ] ],
+          "infoUri": "http://localhost:8111/v1/query/20140120_032523_00000_32v8g",
+          "id": "20140120_032523_00000_32v8g"
+        }
+      };
+
+      var server;
+      var count;
+
+      beforeAll(function(done) {
+        server = http.createServer(function(req, res){
+          if (count === failAfter) {
+            res.statusCode = statusCode;
+          } else {
+            count++;
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.write(JSON.stringify(responses[req.url]));
+          }
+          res.end();
+        });
+        server.listen(8111, function(){
+          done();
+        });
+      });
+
+      beforeEach(function(){
+        count = 0;
+      });
+
+      afterAll(function(done){
+        server.close(function(){
+          done();
+        });
+      });
+
+      test('that client errors', function(done){
+        var client = new Client({
+          host: 'localhost',
+          port: 8111,
+        });
+        client.execute({
+          query: 'SELECT 1 AS col',
+          data: function(){
+            done('should not have data');
+          },
+          callback: function(error){
+            const errorObj = new Error('HTTP error ' + statusCode);
+            expect(error).toEqual(failAfter === 0 ? {
+              "code": statusCode,
+              "error": errorObj,
+              "message": "execution error",
+            } : errorObj);
+            done();
+          },
+        });
+      });
     });
   });
 });
